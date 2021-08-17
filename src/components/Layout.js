2bda832b745cce96d72c165c940bd6459dab5ec2
ganushkevychv <ganushkevychv@gmail.com>
2021-08-17 14:00:25 +0300

    layout added

---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Layout = () => {
+  return (
+    <div id="app" class="flex-container">
+    <div class="block one">
+        <a href="/">Anastasiia Ganushkevych</a>
+    </div>
+    <div class="block two"></div>
+    <div class="block three"></div>
+    <div class="block four"></div>
+  </div>
+  );
+}
+
+export default Layout;
