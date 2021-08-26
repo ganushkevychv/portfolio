aee6b27f0accfce5d4dd93777c9b399fb96f374f
ganushkevychv <ganushkevychv@gmail.com>
2021-08-26 14:23:38 +0300

    About.js component updated

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 const About = () => {
     return(
-        <div>
+        <div className="container-about">
             <p>Welcome</p>
+            <p>on my website!</p>
             <p>I am Anastasiia Ganushkevych</p>
             <p>and I am professional fotographer</p>
             <p>based in Trojmiasto, Poland</p>
+            <p>for more details please<br/></p>
+            <p>visit <a href="https://www.facebook.com/AnastasiiaGanushkevych/" className="about-gallery-link" aria-label="Facebook">GALLERY</a> or</p>
+            <p>contact me via mesenger</p>
         </div>
     )
 }
